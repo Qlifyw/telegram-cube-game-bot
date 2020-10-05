cb5578230fcde7b6c238368fb6c9ae8f316eda1f
Evgen Kopach <zkopach@gmail.com>
2020-10-05 16:09:52 +0300

    init 37

--- src/main/java/org/cubegame/domain/model/message/speach/Appeal.java
+++ src/main/java/org/cubegame/domain/model/message/speach/Appeal.java
@@ -1,11 +1,10 @@
 package org.cubegame.domain.model.message.speach;
 
-public final class Appeal implements Speech {
+final class Appeal implements Speech {
 
     private final String text;
 
-    // TODO make it private
-    public Appeal(final String text) {
+    Appeal(final String text) {
         this.text = text;
     }
 
