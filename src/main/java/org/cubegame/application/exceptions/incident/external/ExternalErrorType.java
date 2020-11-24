7d7d464da0c6d704d8cf783ed61867262d007ab1
Evgen Kopach <zkopach@gmail.com>
2020-11-24 12:25:02 +0200

    init 77

--- src/main/java/org/cubegame/application/exceptions/incident/external/ExternalErrorType.java
+++ src/main/java/org/cubegame/application/exceptions/incident/external/ExternalErrorType.java
@@ -1,19 +1,4 @@
 package org.cubegame.application.exceptions.incident.external;
 
-//public enum ExternalErrorType {
-//
-//    GAME_NOT_FOUND2("1");
-//
-//    private final String value;
-//    private static final String PREFIX = "EE";
-//
-//    ExternalErrorType(final String code) {
-//        this.value = PREFIX + code;
-//    }
-//
-//    public String getValue() {
-//        return value;
-//    }
-//}
-
-public interface ExternalErrorType {}
+public interface ExternalErrorType {
+}
--- src/main/java/org/cubegame/application/exceptions/incident/internal/InternalErrorType.java
+++ src/main/java/org/cubegame/application/exceptions/incident/internal/InternalErrorType.java
@@ -1,33 +1,5 @@
 package org.cubegame.application.exceptions.incident.internal;
 
 
-
-//public enum InternalErrorType {
-//
-//    GAME_NOT_FOUND("1"),
-//    DISK_IO_ERROR("2"),
-//    JSON_MAPPING("3"),
-//    JSON_PARSING("4");
-//
-//    private final String value;
-//    private static final String PREFIX = "IE";
-//
-//    InternalErrorType(final String code) {
-//        this.value = PREFIX + code;
-//    }
-//
-//    public String getValue() {
-//        return value;
-//    }
-//
-//    public static void main(String[] args) {
-//        final ErrorType mapping1 = Fail.Network.Api.REST;
-//        final ErrorType mapping2 = Fail.Network.Io.REST;
-//        final ErrorType mapping3 = Fail.Network.IO;
-//
-//        System.out.println(mapping1.equals(mapping2));
-//    }
-//}
-
 interface InternalErrorType {
 }
