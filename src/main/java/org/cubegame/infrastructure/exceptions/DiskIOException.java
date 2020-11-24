e7f96d74f8e2f7aeef498d66b9dcf63984198bf4
Evgen Kopach <zkopach@gmail.com>
2020-11-24 13:17:17 +0200

    init 83

--- src/main/java/org/cubegame/infrastructure/exceptions/DiskIOException.java
+++
@@ -1,9 +0,0 @@
-package org.cubegame.infrastructure.exceptions;
-
-
-//public final class DiskIOException extends InternalError {
-//
-//    public DiskIOException(final String message, final Exception cause) {
-//        super(InternalErrorType.DISK_IO_ERROR, "IO Incident. " + message, Collections.emptyMap(), cause);
-//    }
-//}
--- src/main/java/org/cubegame/infrastructure/exceptions/NotSupportedEventException.java
+++
@@ -1,8 +0,0 @@
-package org.cubegame.infrastructure.exceptions;
-
-//public final class NotSupportedEventException extends InternalError {
-//
-//    public NotSupportedEventException(final Update update) {
-//        super(InternalErrorType.GAME_NOT_FOUND, "Not supported event. " + update, Collections.emptyMap(),null);
-//    }
-//}
