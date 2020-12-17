bd14db2f4cc9346c16d23cbbfcc4e061e98c9364
Evgen Kopach <zkopach@gmail.com>
2020-12-17 12:33:46 +0200

    init 102

--- src/test/java/org/cubegame/application/handler/phase/StartGamePhaseExecutorIT.java
+++ src/test/java/org/cubegame/application/handler/phase/StartGamePhaseExecutorIT.java
@@ -5,7 +5,6 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.cubegame.application.configuration.TestDatabaseConfiguration;
-import org.cubegame.application.executors.factory.PhaseExecutorFactory;
 import org.cubegame.application.handler.EventHandler;
 import org.cubegame.application.handler.EventHandlerImpl;
 import org.cubegame.application.handler.stepper.CascadePhaseStepper;
@@ -14,6 +13,7 @@ import org.cubegame.application.repositories.game.GameRepository;
 import org.cubegame.application.repositories.round.RoundRepository;
 import org.cubegame.domain.events.Command;
 import org.cubegame.domain.model.dice.Dice;
+import org.cubegame.domain.model.game.Game;
 import org.cubegame.domain.model.identifier.ChatId;
 import org.cubegame.domain.model.identifier.MessageId;
 import org.cubegame.domain.model.identifier.UserId;
@@ -24,7 +24,6 @@ import org.cubegame.infrastructure.model.message.type.ResponseMessage;
 import org.cubegame.infrastructure.properties.ApplicationProperties;
 import org.cubegame.infrastructure.repositories.game.GameRepositoryImpl;
 import org.cubegame.infrastructure.repositories.round.RoundRepositoryImpl;
-import org.cubegame.infrastructure.services.CommandValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -34,6 +33,7 @@ import org.testcontainers.containers.MongoDBContainer;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -45,22 +45,19 @@ class StartGamePhaseExecutorIT {
 
     private final static MongoDBContainer dbContainer = TestDatabaseConfiguration.getInstance();
 
-    private final ConnectionString connectionString = new ConnectionString("mongodb://"+dbContainer.getHost()+":"+dbContainer.getFirstMappedPort());
-    private final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+    private static final ConnectionString connectionString = new ConnectionString("mongodb://"+dbContainer.getHost()+":"+dbContainer.getFirstMappedPort());
+    private static final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
             .retryReads(true)
             .retryWrites(true)
             .build();
 
-    private final MongoClient mongoClient = MongoClients.create(mongoClientSettings);
+    private static final MongoClient mongoClient = MongoClients.create(mongoClientSettings);
 
-    private final RoundRepository roundRepository = new RoundRepositoryImpl(mongoClient);
-    private final GameRepository gameRepository = new GameRepositoryImpl(mongoClient);
+    private static final RoundRepository roundRepository = new RoundRepositoryImpl(mongoClient);
+    private static final GameRepository gameRepository = new GameRepositoryImpl(mongoClient);
 
-    private final CommandValidator commandValidator = new CommandValidator(applicationProperties);
     private final EventHandler eventHandler = new EventHandlerImpl(gameRepository, roundRepository, applicationProperties);
-
-    private final PhaseExecutorFactory phaseExecutorFactory = new PhaseExecutorFactory(gameRepository, roundRepository, commandValidator);
 
     private static final ChatId CHAT_ID = new ChatId(123L);
     private static final UserId USER_ID_1 = new UserId(456L);
@@ -76,21 +73,18 @@ class StartGamePhaseExecutorIT {
 
     @Test
     @DisplayName("Success when all players moved")
-    void suceessWhenAllPlayersMoved() {
+    void successWhenAllPlayersMoved() {
         final Message msgTemplate = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SpeechFactory.EMPTY_SPEECH, null, FORWARDED);
 
         CascadePhaseStepper.moveUp(eventHandler, msgTemplate, commands);
 
-        final Dice firstDice = new Dice(4);
-        final Message message = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SpeechFactory.EMPTY_SPEECH, firstDice, FORWARDED);
+        // first round
+        final Message message = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SpeechFactory.EMPTY_SPEECH, new Dice(4), FORWARDED);
         final List<ResponseMessage> responsesAfterFirstPlayer = eventHandler.handle(message);
-
         assertTrue(responsesAfterFirstPlayer.isEmpty());
 
-        final Dice secondDice = new Dice(2);
-        final Message message2 = new Message(CHAT_ID, USER_ID_2, generateUserName(USER_ID_2), SpeechFactory.EMPTY_SPEECH, secondDice, FORWARDED);
+        final Message message2 = new Message(CHAT_ID, USER_ID_2, generateUserName(USER_ID_2), SpeechFactory.EMPTY_SPEECH, new Dice(2), FORWARDED);
         final List<ResponseMessage> responsesAfterSecondPlayer = eventHandler.handle(message2);
-
         assertFalse(responsesAfterSecondPlayer.isEmpty());
         assertEquals(1, responsesAfterSecondPlayer.size());
 
@@ -116,6 +110,86 @@ class StartGamePhaseExecutorIT {
         final List<ResponseMessage> responsesAfterSecondThrow = eventHandler.handle(message);
 
         assertTrue(responsesAfterSecondThrow.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Skiped when player forward message")
+    void SkipedWhenPlayerForwardMessage() {
+        final Message msgTemplate = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SpeechFactory.EMPTY_SPEECH, null, FORWARDED);
+        CascadePhaseStepper.moveUp(eventHandler, msgTemplate, commands);
+
+        // first round
+        final Message message_round1_player1 = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SPEECH, new Dice(6), FORWARDED);
+        final List<ResponseMessage> responsesR1P1 = eventHandler.handle(message_round1_player1);
+        assertTrue(responsesR1P1.isEmpty());
+
+        final Message message_round1_player2 = new Message(CHAT_ID, USER_ID_2, generateUserName(USER_ID_1), SPEECH, new Dice(2), FORWARDED);
+        final List<ResponseMessage> responsesR1P2 = eventHandler.handle(message_round1_player2);
+        assertFalse(responsesR1P2.isEmpty());
+
+        // second round
+        final Message message_round2_player1 = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SPEECH, new Dice(6), new MessageId(1));
+        final List<ResponseMessage> responsesR2P1 = eventHandler.handle(message_round2_player1);
+        assertTrue(responsesR2P1.isEmpty());
+
+        final Message message_round2_player2 = new Message(CHAT_ID, USER_ID_2, generateUserName(USER_ID_1), SPEECH, new Dice(2), FORWARDED);
+        final List<ResponseMessage> responsesR2P2 = eventHandler.handle(message_round2_player2);
+        assertTrue(responsesR2P2.isEmpty());
+
+        final Message message_round2_player1_next = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SPEECH, new Dice(4), FORWARDED);
+        final List<ResponseMessage> responsesR2P1_next = eventHandler.handle(message_round2_player1_next);
+        assertFalse(responsesR2P1_next.isEmpty());
+
+    }
+
+    @Test
+    @DisplayName("Can cancel game")
+    void canCancellGame() {
+        final Message msgTemplate = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SpeechFactory.EMPTY_SPEECH, null, FORWARDED);
+        CascadePhaseStepper.moveUp(eventHandler, msgTemplate, commands);
+
+        // first throw
+        final Message message_round1_player1 = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SPEECH, null, FORWARDED);
+        final List<ResponseMessage> responsesR1P1 = eventHandler.handle(message_round1_player1);
+        assertTrue(responsesR1P1.isEmpty());
+
+        // cancel game
+        final Speech cancelCommand = speechFactory.of(String.format("%s@%s", Command.STOP.getValue(), BOT_NAME));
+        final Message message_round1_player2 = new Message(CHAT_ID, USER_ID_2, generateUserName(USER_ID_1), cancelCommand, null, FORWARDED);
+        final List<ResponseMessage> responsesR1P2 = eventHandler.handle(message_round1_player2);
+        assertTrue(responsesR1P2.isEmpty());
+
+        Optional<Game> activeGames = gameRepository.getActive(CHAT_ID);
+        assertFalse(activeGames.isPresent());
+    }
+
+    @Test
+    @DisplayName("Game successfully completed")
+    void gameSuccessfullyCompleted() {
+        final Message msgTemplate = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SpeechFactory.EMPTY_SPEECH, null, FORWARDED);
+        CascadePhaseStepper.moveUp(eventHandler, msgTemplate, commands);
+
+        // first round
+        final Message message_round1_player1 = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SPEECH, new Dice(6), FORWARDED);
+        final List<ResponseMessage> responsesR1P1 = eventHandler.handle(message_round1_player1);
+        assertTrue(responsesR1P1.isEmpty());
+
+        final Message message_round1_player2 = new Message(CHAT_ID, USER_ID_2, generateUserName(USER_ID_1), SPEECH, new Dice(2), FORWARDED);
+        final List<ResponseMessage> responsesR1P2 = eventHandler.handle(message_round1_player2);
+        assertFalse(responsesR1P2.isEmpty());
+
+        // second round
+        final Message message_round2_player1 = new Message(CHAT_ID, USER_ID_1, generateUserName(USER_ID_1), SPEECH, new Dice(6), FORWARDED);
+        final List<ResponseMessage> responsesR2P1 = eventHandler.handle(message_round2_player1);
+        assertTrue(responsesR2P1.isEmpty());
+
+        final Message message_round2_player2 = new Message(CHAT_ID, USER_ID_2, generateUserName(USER_ID_1), SPEECH, new Dice(3), FORWARDED);
+        final List<ResponseMessage> responsesR2P2 = eventHandler.handle(message_round2_player2);
+        assertFalse(responsesR2P2.isEmpty());
+
+        // check game completed
+        Optional<Game> activeGames = gameRepository.getActive(CHAT_ID);
+        assertFalse(activeGames.isPresent());
     }
 
     @ParameterizedTest(name = "Skip {0} message")
